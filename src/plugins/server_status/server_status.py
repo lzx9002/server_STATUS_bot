e79c1b1efa915ab9e34d47b591eb1b4a1446c165
lzx20129002 <lzx20129002@163.com>
2025-04-05 22:34:17 +0800

    基础功能全部完工！

--- src/plugins/server_status/server_status.py
+++ src/plugins/server_status/server_status.py
@@ -10,16 +10,21 @@ from nonebot.adapters.onebot.v11 import Bot as V11Bot, MessageEvent, PokeNotifyE
 from nonebot.permission import SUPERUSER
 from nonebot.plugin.on import on_message, on_command, on_notice
 
-from src.plugins.server_status.utils import call_bt_status_api, get_status
+from src.plugins.server_status.utils import call_bt_status_api, get_status, formatted
 
-status = on_command("状态", aliases={"status"}, permission=SUPERUSER)
-status_Poke = on_notice(permission=SUPERUSER)
+status = on_command("状态", aliases={"status"})
+status_Poke = on_notice()
 
 @status.handle()
+@status_Poke.handle()
 async def _(event: MessageEvent | PokeNotifyEvent, bot: V11Bot):
     data =get_status()
     disk = ''
     for key, i in data["disk"].items():
         disk += f"\t{key}:{i}%\n"
     await status.send(f"cpu占用:{data["cpu"]}%\n内存占用:{data["memory"]}%\n虚拟内存:{data["swap"]}%\n磁盘占用:\n{disk}")
-    await status.finish(json.dumps(call_bt_status_api(), indent=4).encode('utf-8').decode('unicode_escape'))
+    bt_data = call_bt_status_api()
+    if bt_data.get('status', True):
+        await status.finish(formatted(call_bt_status_api()))
+    else:
+        await status.finish(bt_data['msg'].encode('utf-8').decode('unicode_escape'))
