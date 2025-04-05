64bccef6491397c5019e88552a953cd0016ca4ff
dison0331-ThinkPad <139662128+dison0331@users.noreply.github.com>
2025-04-05 21:36:32 +0800

    破防

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 # @Project : server_STATUS_bot
-# @File    : test.py
+# @File    : utils.py
 # @IDE     : PyCharm
 # @Author  : lzx9002
-# @Time    : 2025/4/5 17:29
+# @Time    : 2025/4/5 14:46
 import hashlib
-import json
 import time
 
 import psutil
@@ -24,8 +23,16 @@ def call_bt_status_api():
         'request_token': md5(request_time + md5(api_sk)),
         'request_time': request_time
     }
-    print(p_data)
     x = requests.post(url="https://hk.yousb.top:8888/system?action=GetNetWork",data=p_data)
     res = x.json()
     return res
-print(json.dumps(call_bt_status_api()))
+def get_status():
+    return {
+        "cpu": psutil.cpu_percent(),
+        "memory": psutil.virtual_memory().percent,
+        "disk": {d.mountpoint: usage.percent
+        for d in psutil.disk_partitions()
+        if (usage := psutil.disk_usage(d.mountpoint))
+                 },
+        "swap": psutil.swap_memory().percent,
+    }
