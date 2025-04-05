5182f50c31ea23523a30ed4b4b80ded00671f4a1
lzx20129002 <lzx20129002@163.com>
2025-04-05 17:59:56 +0800

    来帮修bug

--- test.py
+++ test.py
@@ -14,9 +14,9 @@ def call_bt_status_api():
     api_sk="bu2DutNRkS5yFZTjlPni6sXFVh7LHkWh"
     request_time=int(time.time())
     def md5(s:str):
-        return hashlib.md5(s.encode()).hexdigest()
-    request_token = md5(str(request_time))+md5(api_sk)
-    x = requests.post("http://159.138.156.179:8888/system?action=GetSystemTotal",params={"request_time": request_time, " request_token": request_token})
+        return hashlib.md5(s.encode('utf-8')).hexdigest()
+    request_token = md5(str(request_time)+''+md5(api_sk))
+    x = requests.post("http://159.138.156.179:8888/system?action=GetSystemTotal",json={"request_time": request_time, " request_token": request_token})
     res = x.text
     return res
 print(call_bt_status_api())
